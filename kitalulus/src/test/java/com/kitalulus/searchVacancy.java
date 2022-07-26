7094829eb443429e7d3d629043fe432dd53d2e8f
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-07-27 00:07:35 +0700

    selenium without screenshot

--- kitalulus/src/test/java/com/kitalulus/searchVacancy.java
+++ kitalulus/src/test/java/com/kitalulus/searchVacancy.java
@@ -1,21 +1,11 @@
 package com.kitalulus;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
-
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
-import org.testng.Reporter;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -24,17 +14,6 @@ public class searchVacancy {
 
 	WebDriver driver;
 	String baseURL = "https://kerja.kitalulus.com/id";
-
-	public static File ambilGambar(WebDriver webdriver, String filePath) throws IOException {
-		TakesScreenshot screenShoot = ((TakesScreenshot) webdriver);
-		File srcFile = screenShoot.getScreenshotAs(OutputType.FILE);
-		File destinationFile = new File(filePath);
-
-		// copy file at destination
-		FileUtils.copyFile(srcFile, destinationFile);
-
-		return destinationFile;
-	}
 
 	@BeforeSuite
 	public void setupDriver() {
@@ -60,22 +39,6 @@ public class searchVacancy {
 				Assert.assertEquals(result.contains(expect), true);
 				state = false;
 			} catch (Exception e) {
-			}
-		}
-
-		state = true;
-		while (state) {
-			try {
-				// file path
-				String akhirFile = System.getProperty("user.dir") + "\\test-output\\searchVacancy.png";
-				File destinationFile = searchVacancy.ambilGambar(driver, akhirFile);
-
-				// report code
-				Reporter.log("<a target='_blank' href=' " + destinationFile.getAbsolutePath() + "'>" + "'<img src='"
-						+ destinationFile.getAbsolutePath() + "'width=100 height=100/></a>");
-				state = false;
-			} catch (IOException e) {
-				System.out.println("ada eror bang");
 			}
 		}
 
