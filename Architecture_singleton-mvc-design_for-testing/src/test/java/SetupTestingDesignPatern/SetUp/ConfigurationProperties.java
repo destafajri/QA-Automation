7ea83e398fc409c89e4052d38679283194e143ad
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-08-12 21:13:21 +0700

    configuration

--- Architecture_singleton-mvc-design_for-testing/src/test/java/SetupTestingDesignPatern/SetUp/ConfigurationProperties.java
+++ Architecture_singleton-mvc-design_for-testing/src/test/java/SetupTestingDesignPatern/SetUp/ConfigurationProperties.java
@@ -1,5 +1,10 @@
 package SetupTestingDesignPatern.SetUp;
 
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("framework.properties")
 public class ConfigurationProperties {
 
 }
