784c9cd3e2ae9f3a305ab0bcf00e8e3e7133e28d
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-08-14 13:35:58 +0700

    try to run using existing chrome

--- 08-TokopediaBDDtest_with_singleton-mvc-architecture_existingReport/src/test/java/SetupTesting/SetupDriverSingleton/ChromeExistingBrowser.java
+++ 08-TokopediaBDDtest_with_singleton-mvc-architecture_existingReport/src/test/java/SetupTesting/SetupDriverSingleton/ChromeExistingBrowser.java
@@ -7,6 +7,18 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class ChromeExistingBrowser implements DriverStrategyInterface {
 
 	public WebDriver setStrategy() {
+		/*
+		 * Navigate to chrome directory using the cd command In my case chrome.exe is
+		 * under C:\Program Files (x86)\Google\Chrome\Application
+		 *  ==> cd C:\Program Files (x86)\Google\Chrome\Application
+		 * 
+		 * Syntax chrome.exe –remote-debugging-port=any-free-port
+		 * –user-data-dir=directory (path where you need to store data)
+		 * 
+		 * Example in my case
+		 * 	==> chrome.exe --remote-debugging-port=9222 --user-data-dir=C:\chromeData
+		 */
+		
 		String path = System.getenv("WEBDRIVER");
 		System.setProperty("webdriver.chrome.driver", path);
 		// Create object of ChromeOptions Class
