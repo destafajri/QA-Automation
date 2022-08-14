6da6fb56d86c8a742bfa2c94bb48f6dcf0f89340
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-08-14 17:29:33 +0700

    scrap product element i forgot how to return the array list

--- 08-TokopediaBDDtest_with_singleton-mvc-architecture_existingReport/src/test/java/SetupTesting/SetupElementTesting/ScrapProductElement.java
+++ 08-TokopediaBDDtest_with_singleton-mvc-architecture_existingReport/src/test/java/SetupTesting/SetupElementTesting/ScrapProductElement.java
@@ -1,5 +1,7 @@
 package SetupTesting.SetupElementTesting;
 
+import java.util.List;
+
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -22,7 +24,10 @@ public class ScrapProductElement {
 	private WebElement nameAll;
 	
 	@FindBy(xpath = "//div[@data-testid='spnSRPProdPrice']")
-	private WebElement priceAll;
+	private List<WebElement> priceAll;
+	
+	@FindBy(xpath = "//button[@aria-label='Laman berikutnya']")
+	private WebElement next;
 
 	
 	// method
@@ -31,6 +36,32 @@ public class ScrapProductElement {
 		for (int y = 0; y <= 80; y++) {
 			js.executeScript("window.scrollBy(0," + y + ")");
 		}
+	}
+	
+	public void page(int page) {
+		for (int i = 0; i < page; i++) {
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			js.executeScript("arguments[0].click()", next);
+			this.scroll();
+		}
+	}
+	
+	public String priceAllStr() {
+		List<WebElement> priceAll = this.priceAll;
+		for(WebElement price: priceAll) {
+			return price.getText();
+		}
+		return null;
+	}
+	
+	public int priceAllInt() {
+		List<WebElement> priceAll = this.priceAll;
+		for (int x = 0; x < priceAll.size(); x++) {
+			priceAll.get(x).getText();
+			int price = Integer.valueOf(priceAll.get(x).getText().replace(".", "").replace(" ", ""));
+			return price;
+		}
+		return (Integer) null;
 	}
 
 }
